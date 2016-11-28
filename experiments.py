cf63a130950d1d057df7178b189e0342ec4d6bf8
Remi Baar <remibaar@gmail.com>
2016-11-28 20:33:42 +0100

    Rename experiments

--- experiments.py
+++ experiments.py
@@ -6,25 +6,49 @@ import logging
 logging.basicConfig(level=logging.INFO)
 experiments = {
     'das_sarma_test': experiment.Experiment(
-                edgelist='data/test.txt',
-                precomputation_dir='/tmp/sketches_test',
-                precomputation=das_sarma.precomputation,
-                precomputation_kwargs={'k':1},
-                number_of_checks=32,
-                computation=das_sarma.sketch,
-                force_precomputation=True,
-                id='test'
-            ),
-    'gubichev_test': experiment.Experiment(
-                edgelist='data/test.txt',
-                precomputation_dir='/tmp/sketches_test',
-                precomputation=gubichev.precomputation,
-                precomputation_kwargs={'k': 1},
-                number_of_checks=32,
-                computation=gubichev.convert_queue_to_length,
-                computation_kwargs={'function': gubichev.sketch_cesc},
-                force_precomputation=True,
-                id='test'
-            ),
+        edgelist='data/test.txt',
+        precomputation_dir='/tmp/sketches/das_sarma_test',
+        precomputation=das_sarma.precomputation,
+        precomputation_kwargs={'k': 1},
+        number_of_checks=32,
+        computation=das_sarma.sketch,
+        force_precomputation=True,
+        id='test'
+    ),
+    'gubichev_slashdot_cesc': experiment.Experiment(
+        edgelist='data/slashdot.txt',
+        precomputation_dir='/tmp/sketches/gubichev_slashdot_cesc',
+        precomputation=gubichev.precomputation,
+        precomputation_kwargs={'k': 2},
+        number_of_checks=1000,
+        computation=gubichev.convert_queue_to_length,
+        computation_kwargs={'function': gubichev.sketch_cesc},
+        force_precomputation=False,
+        id='gubichev_slashdot_cesc'
+    ),
+
+    'gubichev_slashdot_ce': experiment.Experiment(
+        edgelist='data/slashdot.txt',
+        precomputation_dir='/tmp/sketches/gubichev_slashdot_ce',
+        precomputation=gubichev.precomputation,
+        precomputation_kwargs={'k': 2},
+        number_of_checks=1000,
+        computation=gubichev.convert_queue_to_length,
+        computation_kwargs={'function': gubichev.sketch_ce},
+        force_precomputation=False,
+        id='gubichev_slashdot_ce'
+    ),
+
+    'gubichev_slashdot_sketch': experiment.Experiment(
+        edgelist='data/slashdot.txt',
+        precomputation_dir='/tmp/sketches/gubichev_slashdot_ce',
+        precomputation=gubichev.precomputation,
+        precomputation_kwargs={'k': 2},
+        number_of_checks=1000,
+        computation=gubichev.convert_queue_to_length,
+        computation_kwargs={'function': gubichev.sketch},
+        force_precomputation=False,
+        id='gubichev_slashdot_ce'
+    ),
 
 }
