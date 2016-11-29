bb665f34e4302a7a511b2fe417b10f850281d1be
bvdboom <bvdboom@gmail.com>
2016-11-29 20:52:08 +0100

    Constrained closeness centrality

--- potamias.py
+++ potamias.py
@@ -1,3 +1,6 @@
+import networkx as nx
+from random import choice
+
 def random_landmarks(g,d):
     """
     :param g: Graph
@@ -17,32 +20,46 @@ def deg_centrality_landmarks(g,d):
     s = i.sorted(reverse=True, key=lambda x: x[1])
     return map(lambda x: x[0], s[:d])
 
-def closeness_centrality_landmarks(g, k, d):
+def closeness_centrality_ls(g, k, d, h=None, constrain=False):
     """
     :param g: Graph
     :param k: Number of nodes to randomly sample and solve SSSP for
     :param d: Top d landmarks to return
+    :param h: Discard nodes that are at distance h from selected landmark
     :return: A sorted list in ascending order of the top d lowest
              closeness centralities of all the nodes in g, and
-             all closeness centralities
+             all unsorted closeness centralities
     """
     v = []
-    sp = dict()
+    sp, closeness = {}, {}
     n = g.n()
-    closeness = {}
-    # Calculate shortest paths from random node v_i to all other nodes and store in dictionary with key = v_i
+    # Calculate shortest path lengths from random node v_i
+    # to all other nodes and store in dictionary with key = v_i
     for i in range(k):
         v.append(choice(g.nodes()))
         sp[v[i]] = nx.shortest_path_length(g, source=v[i])
-    # For all nodes j in g, calculate centrality estimate based on Eppstein & Wang and store in closeness[j]
+    # For all nodes j in g, calculate centrality estimate
+    # based on Eppstein & Wang and store in closeness[j]
     for j in g.nodes():
         sum = 0
         for c in range(k):
             sum += n*sp[v[c]][j]/(k*(n-1))
-        if sum == 0:
-            print(sum)
         closeness[j] = sum
     # Return the top d
-    q = sorted(closeness, key=closeness.get)
-    return q[:d], closeness
+    sorted_cc = sorted(closeness, key=closeness.get)
+    # Constrained closeness centrality
+    if constrain:
+        count = d
+        not_a_landmark = []
+        con = {}
+        for i in q:
+            if i not in not_a_landmark:
+                con[sorted_cc[i]] = closeness[sorted_cc[i]]
+                count -= 1
+                not_a_landmark = list(nx.single_source_shortest_path_length(g, source=i, cutoff=h))
+            if count == 0:
+                return con, closeness
+    return sorted_cc[:d], closeness
 
+# Partitions
+# https://github.com/valiantljk/graph-partition/blob/master/algorithms/boundary.py
