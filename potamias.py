e24055ef174656e6b5df17dac5f966984522e6c4
bvdboom <bvdboom@gmail.com>
2016-12-02 00:01:58 +0100

    Rewrite of some functions/code
    To be done: docstrings

--- potamias.py
+++ potamias.py
@@ -1,65 +1,50 @@
 import networkx as nx
 from random import choice
 
-def random_landmarks(g,d):
-    """
-    :param g: Graph
-    :param d: Number of landmarks to return
-    :return: d random nodes
-    """
-    return g.random_nodes(d)
-
-def deg_centrality_landmarks(g,d):
-    """
-    :param g: Graph
-    :param d: Number of landmarks to return
-    :return: Return top d degree centrality nodes
-    """
-    dc = nx.degree_centrality()
-    i = dc.items()
-    s = i.sorted(reverse=True, key=lambda x: x[1])
-    return map(lambda x: x[0], s[:d])
-
-def closeness_centrality_ls(g, k, d, h=None, constrain=False):
-    """
-    :param g: Graph
-    :param k: Number of nodes to randomly sample and solve SSSP for
-    :param d: Top d landmarks to return
-    :param h: Discard nodes that are at distance h from selected landmark
-    :return: A sorted list in ascending order of the top d lowest
-             closeness centralities of all the nodes in g, and
-             all unsorted closeness centralities
-    """
+def select_nodes(g, k, d, h=None, constrained=False, strategy='closeness'):
     v = []
-    sp, closeness = {}, {}
-    n = g.n()
-    # Calculate shortest path lengths from random node v_i
-    # to all other nodes and store in dictionary with key = v_i
+    sp = {}
+    # Calculate shortest paths from random node v_i to all other nodes and store in dictionary with key = v_i
     for i in range(k):
         v.append(choice(g.nodes()))
         sp[v[i]] = nx.shortest_path_length(g, source=v[i])
-    # For all nodes j in g, calculate centrality estimate
-    # based on Eppstein & Wang and store in closeness[j]
+    # For all nodes j in g, calculate centrality estimate based on Eppstein & Wang and store in closeness[j]
+    if strategy == 'random':
+        return g.random_nodes(d)
+    elif strategy == 'degree':
+        node_centrality = nx.degree_centrality(g)
+        sorted_c = sorted(node_centrality, reverse=True, key=node_centrality.get)
+        selected = sorted_c[:d]
+    elif strategy == 'closeness':
+        node_centrality = approx_closeness(g, k, v, sp)
+        sorted_c = sorted(node_centrality, key=node_centrality.get)
+        selected = sorted_c[:d]
+    # Return the top d
+    if constrained:
+        selected = constrain(node_centrality, sorted_c, d,h)
+    return selected
+
+def approx_closeness(g, k, v, sp):
+    n = g.number_of_nodes()
+    closeness = {}
     for j in g.nodes():
         sum = 0
         for c in range(k):
             sum += n*sp[v[c]][j]/(k*(n-1))
         closeness[j] = sum
-    # Return the top d
-    sorted_cc = sorted(closeness, key=closeness.get)
-    # Constrained closeness centrality
-    if constrain:
-        count = d
-        not_a_landmark = []
-        con = {}
-        for i in q:
-            if i not in not_a_landmark:
-                con[sorted_cc[i]] = closeness[sorted_cc[i]]
-                count -= 1
-                not_a_landmark = list(nx.single_source_shortest_path_length(g, source=i, cutoff=h))
-            if count == 0:
-                return con, closeness
-    return sorted_cc[:d], closeness
+    return closeness
+
+def constrain(node_centrality, sorted_c, d, h):
+    count = d
+    not_a_landmark = []
+    con = {}
+    for i in sorted_c:
+        if i not in not_a_landmark:
+            con[i] = node_centrality[sorted_c[i]]
+            not_a_landmark = list(nx.single_source_shortest_path_length(g, source=i, cutoff=h))
+            count -= 1
+        if count == 0:
+            return list(con.keys())
 
 # Partitions
 # https://github.com/valiantljk/graph-partition/blob/master/algorithms/boundary.py
