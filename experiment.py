fd259d561a218507714d3f32afb803a6572f5a38
Remi Baar <remibaar@gmail.com>
2016-12-07 11:10:15 +0100

    Rewritten experiment.py

--- experiment.py
+++ experiment.py
@@ -2,32 +2,141 @@ import graph
 import os
 import logging
 import timeit
+import config
+import pandas as pd
 
+
+class Experiment:
+
+    def __init__(self, id, edgelist, computations, precomputation_func, precomputation_kwargs = {}, number_of_checks = 1, number_of_iterations = 1, graph_processor = graph.NetworkxGraph):
+        self.precomputation_results = pd.DataFrame(columns=['id', 'time', 'dir_size'])
+        self.results = pd.DataFrame(columns=['s', 'd', 'dijkstra_distance', 'dijkstra_time'])
+
+        self.number_of_iterations = number_of_iterations
+        self.number_of_checks = number_of_checks
+
+        self.precomputation_func = precomputation_func
+        self.precomputation_kwargs = precomputation_kwargs
+        self.id = id
+
+        self.computations = computations
+
+        self.edgelist = edgelist
+        self.graph_processor = graph_processor
+        self._load_graph()
+
+
+
+    def _load_graph(self):
+        self.graph = self.graph_processor()
+        self.graph.read_edgelist(self.edgelist)
+
+
+    def precomputation(self, dir, id):
+        # make dir if not exists
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+
+        tick = timeit.default_timer()
+        self.precomputation_func(self.graph, dir, **self.precomputation_kwargs)
+        tock = timeit.default_timer()
+
+
+        # Calculate results and add to data set
+        time = tock-tick
+        dir_size = sum([os.path.getsize(dir+'/'+f) for f in os.listdir(dir) if os.path.isfile(dir+'/'+f)])
+
+        result = pd.DataFrame([{'id': id, 'time': time, 'dir_size': dir_size}])
+
+        self.precomputation_results = self.precomputation_results.append(result, ignore_index=True)
+
+
+    def determine_test_set(self):
+
+        results = pd.DataFrame(columns=['s', 'd', 'dijkstra_distance', 'dijkstra_time'])
+
+        for i in range(self.number_of_checks):
+            s = self.graph.random_node()
+            d = self.graph.random_node()
+            tic = timeit.default_timer()
+            real_distance = self.graph.shortest_path_length(s, d)
+            toc = timeit.default_timer
+
+            time = toc - tic
+            results.loc[i] = [s, d, real_distance, time]
+
+        return results
+
+
+    def computation(self, id, results, precomputation_dir, function, function_kwargs):
+
+
+        new_results = pd.DataFrame(columns=[id+'_time', id+'_approximation'])
+        for i, row in results.iterrows():
+            s = row['s']
+            d = row['d']
+
+            tick = timeit.default_timer()
+            approximation = function(s, d, self.graph, precomputation_dir, **function_kwargs)
+            tock = timeit.default_timer()
+
+            # Calculate results and add to data set
+            time = tock - tick
+
+            new_results.loc[i] = [time, approximation]
+
+        return pd.concat([results, new_results], axis=1)
+
+
+
+    def run(self):
+
+        results_dir = config.result_dir + '/' + str(self.id) + '/'
+
+        # make dir if not exists
+        if not os.path.exists(results_dir):
+            os.makedirs(results_dir)
+
+        for i in range(self.number_of_iterations):
+            sketch_dir = config.sketch_dir+'/'+str(self.id)+'/'+str(i)+'/'
+            self.precomputation(sketch_dir, i)
+
+
+            results = self.determine_test_set()
+            for id, (func, kwargs) in self.computations.items():
+                results = self.computation(id, results, sketch_dir, func, kwargs)
+
+
+            results.to_excel(results_dir+str(i)+'.xlsx')
+
+        self.precomputation_results.to_excel(results_dir+'precomputation.xlsx')
+
+"""
 class Experiment:
 
     def __init__(
             self,
             id,
             edgelist,
-            computation,
             number_of_checks,
-            computation_kwargs = dict(),
-            graph_processor=graph.NetworkxGraph,
+            computations,
+            number_of_iterations=1,
             precomputation=None,
-            precomputation_dir=None,
             precomputation_kwargs=dict(),
-            force_precomputation=False
+            graph_processor=graph.NetworkxGraph
     ):
         self.id = id
         self.edgelist = edgelist
         self.graph_processor = graph_processor
-        self.computation_func = computation
+        self.computations = computations
         self.precomputation_func = precomputation
         self.precomputation_kwargs = precomputation_kwargs
-        self.precomputation_dir = precomputation_dir
-        self.force_precomputation = force_precomputation
-        self.computation_kwargs = computation_kwargs
         self.number_of_checks = number_of_checks
+        self.number_of_iterations = number_of_iterations
+
+        self.precomputation_time = list()
+        self.computation_results = list()
+        self.computation_time = list()
 
     def _load_graph(self):
         self.graph = self.graph_processor()
@@ -58,7 +167,7 @@ class Experiment:
         # Create flag file, to indicate precomputation was completed
         open(self.precomputation_dir+'/.precompation_completed', mode='w+').close()
 
-    def computation(self):
+    def computation(self, function, function_kwargs):
         good_results = list()
 
         logging.info('Start selection of random nodes and distance calculation')
@@ -77,7 +186,6 @@ class Experiment:
         tick = timeit.default_timer()
         for (s, d, real_distance) in good_results:
             approximations.append(self.computation_func(s, d, self.graph, self.precomputation_dir, **self.computation_kwargs))
-            #print(s, ' -> ', d, ' = ', approx_distance, '(real distance = ', real_distance, ')')
         tock = timeit.default_timer()
         logging.info('Finished approximations in '+str(tock-tick)+'s')
 
@@ -85,13 +193,19 @@ class Experiment:
         total_approx_error = 0
         for i in range(self.number_of_checks):
             (s, d, real_distance) = good_results[i]
-            total_approx_error += (abs(real_distance - approximations[i]) + 1) / (real_distance + 1)
+
+            if (real_distance != float('inf') or approximations[i] != float('inf')) and (real_distance > 0) :
+                total_approx_error += abs(real_distance - approximations[i]) / real_distance
+
         avg_approx_error = total_approx_error / self.number_of_checks
+
 
         print('Average approximation error is ' + str(avg_approx_error))
         logging.info('Finished Start calculation metrics')
 
     def run(self):
         self._load_graph()
-        self.precomputation()
-        self.computation()
+
+        for i in range(self.number_of_checks):
+            self.precomputation()
+            self.computation() """
