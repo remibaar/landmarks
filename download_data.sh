74a789d3725a6400dfdad297cde08e38d83e6483
Remi Baar <remibaar@gmail.com>
2016-12-13 14:43:33 +0100

    Add facebook and roadnet_ca to datasets

--- download_data.sh
+++ download_data.sh
@@ -2,6 +2,9 @@ declare -A sources
 sources["slashdot.txt"]="https://snap.stanford.edu/data/soc-Slashdot0811.txt.gz"
 sources["google.txt"]="https://snap.stanford.edu/data/web-Google.txt.gz"
 sources["gplus.txt"]="https://snap.stanford.edu/data/gplus_combined.txt.gz"
+sources["facebook.txt"]="https://snap.stanford.edu/data/facebook_combined.txt.gz"
+sources["roadnet_ca.txt"]="https://snap.stanford.edu/data/roadNet-CA.txt.gz"
+
 
 
 for key in ${!sources[@]}; do
