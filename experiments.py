66ce40011fcbb24cba8899da7e74b2311cf47fcc
Remi Baar <remibaar@gmail.com>
2016-12-13 14:54:16 +0100

    Experiments  creation in for loop

--- experiments.py
+++ experiments.py
@@ -5,117 +5,35 @@ import logging
 import potamias
 
 logging.basicConfig(level=logging.INFO)
-experiments = {
-    'gubichev_k1_slashdot': experiment.Experiment(id='gubichev_k1_slashdot', edgelist='slashdot.txt',
-                                                  number_of_iterations=20,
-                                                  number_of_checks=1000,
-                                                  precomputation_func=gubichev.precomputation,
-                                                  precomputation_kwargs={'k': 1},
-                                                  computations={
-                                                      'sketch': (
-                                                          gubichev.convert_queue_to_length,
-                                                          {'function': gubichev.sketch}),
-                                                      'sketch_ce': (
-                                                          gubichev.convert_queue_to_length,
-                                                          {'function': gubichev.sketch_ce}),
-                                                      'sketch_cesc': (
-                                                          gubichev.convert_queue_to_length,
-                                                          {'function': gubichev.sketch_cesc}),
-                                                      'tree_sketch': (
-                                                          gubichev.convert_queue_to_length,
-                                                          {'function': gubichev.tree_sketch}),
-                                                  }),
 
-    'gubichev_k2_slashdot': experiment.Experiment(id='gubichev_k2_slashdot', edgelist='slashdot.txt',
-                                                  number_of_iterations=20,
-                                                  number_of_checks=1000,
-                                                  precomputation_func=gubichev.precomputation,
-                                                  precomputation_kwargs={'k': 2},
-                                                  computations={
-                                                      'sketch': (
-                                                          gubichev.convert_queue_to_length,
-                                                          {'function': gubichev.sketch}),
-                                                      'sketch_ce': (
-                                                          gubichev.convert_queue_to_length,
-                                                          {'function': gubichev.sketch_ce}),
-                                                      'sketch_cesc': (
-                                                          gubichev.convert_queue_to_length,
-                                                          {'function': gubichev.sketch_cesc}),
-                                                      'tree_sketch': (
-                                                          gubichev.convert_queue_to_length,
-                                                          {'function': gubichev.tree_sketch}),
-                                                  }),
-    'gubichev_k1_gplus': experiment.Experiment(id='gubichev_k1_gplus', edgelist='gplus.txt', number_of_iterations=20,
-                                               number_of_checks=1000,
-                                               precomputation_func=gubichev.precomputation,
-                                               precomputation_kwargs={'k': 1},
-                                               computations={
-                                                   'sketch': (
-                                                       gubichev.convert_queue_to_length, {'function': gubichev.sketch}),
-                                                   'sketch_ce': (
-                                                       gubichev.convert_queue_to_length,
-                                                       {'function': gubichev.sketch_ce}),
-                                                   'sketch_cesc': (
-                                                       gubichev.convert_queue_to_length,
-                                                       {'function': gubichev.sketch_cesc}),
-                                                   'tree_sketch': (
-                                                       gubichev.convert_queue_to_length,
-                                                       {'function': gubichev.tree_sketch}),
-                                               }),
+data_sets = [
+    "slashdot",
+    "google",
+    "gplus",
+    "facebook",
+    "roadnet_ca"
+]
 
-    'gubichev_k2_gplus': experiment.Experiment(id='gubichev_k2_gplus', edgelist='gplus.txt', number_of_iterations=20,
-                                               number_of_checks=1000,
-                                               precomputation_func=gubichev.precomputation,
-                                               precomputation_kwargs={'k': 2},
-                                               computations={
-                                                   'sketch': (
-                                                       gubichev.convert_queue_to_length, {'function': gubichev.sketch}),
-                                                   'sketch_ce': (
-                                                       gubichev.convert_queue_to_length,
-                                                       {'function': gubichev.sketch_ce}),
-                                                   'sketch_cesc': (
-                                                       gubichev.convert_queue_to_length,
-                                                       {'function': gubichev.sketch_cesc}),
-                                                   'tree_sketch': (
-                                                       gubichev.convert_queue_to_length,
-                                                       {'function': gubichev.tree_sketch}),
-                                               }),
-    'gubichev_k1_google': experiment.Experiment(id='gubichev_k1_google', edgelist='google.txt', number_of_iterations=20,
-                                                number_of_checks=1000,
-                                                precomputation_func=gubichev.precomputation,
-                                                precomputation_kwargs={'k': 1},
-                                                computations={
-                                                    'sketch': (
-                                                        gubichev.convert_queue_to_length,
-                                                        {'function': gubichev.sketch}),
-                                                    'sketch_ce': (
-                                                        gubichev.convert_queue_to_length,
-                                                        {'function': gubichev.sketch_ce}),
-                                                    'sketch_cesc': (
-                                                        gubichev.convert_queue_to_length,
-                                                        {'function': gubichev.sketch_cesc}),
-                                                    'tree_sketch': (
-                                                        gubichev.convert_queue_to_length,
-                                                        {'function': gubichev.tree_sketch}),
-                                                }),
+experiments = dict()
 
-    'gubichev_k2_google': experiment.Experiment(id='gubichev_k2_google', edgelist='google.txt', number_of_iterations=20,
-                                                 number_of_checks=1000,
-                                                 precomputation_func=gubichev.precomputation,
-                                                 precomputation_kwargs={'k': 2},
-                                                 computations={
-                                                     'sketch': (
-                                                         gubichev.convert_queue_to_length,
-                                                         {'function': gubichev.sketch}),
-                                                     'sketch_ce': (
-                                                         gubichev.convert_queue_to_length,
-                                                         {'function': gubichev.sketch_ce}),
-                                                     'sketch_cesc': (
-                                                         gubichev.convert_queue_to_length,
-                                                         {'function': gubichev.sketch_cesc}),
-                                                     'tree_sketch': (
-                                                         gubichev.convert_queue_to_length,
-                                                         {'function': gubichev.tree_sketch}),
-                                                 }),
-
-}
+for data in data_sets:
+    id = 'gubichev_' + data
+    experiments[id] = experiment.Experiment(id=id, edgelist=data + '.txt',
+                                            number_of_iterations=20,
+                                            number_of_checks=1000,
+                                            precomputation_func=gubichev.precomputation,
+                                            precomputation_kwargs={'k': 2},
+                                            computations={
+                                                'sketch': (
+                                                    gubichev.convert_queue_to_length,
+                                                    {'function': gubichev.sketch}),
+                                                'sketch_ce': (
+                                                    gubichev.convert_queue_to_length,
+                                                    {'function': gubichev.sketch_ce}),
+                                                'sketch_cesc': (
+                                                    gubichev.convert_queue_to_length,
+                                                    {'function': gubichev.sketch_cesc}),
+                                                'tree_sketch': (
+                                                    gubichev.convert_queue_to_length,
+                                                    {'function': gubichev.tree_sketch}),
+                                            }),
