a1af852c434a8c73e76c2b20eac799a720afcd5e
Remi Baar <remibaar@gmail.com>
2016-12-13 17:13:04 +0100

    Results processor

---
+++ process_results.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import config
+import logging
+
+ids = [
+    'gubichev_facebook', 'gubichev_google', 'gubichev_gplus', 'gubichev_roadnet_ca', 'gubichev_slashdot'
+]
+
+precomputation_results = pd.DataFrame(columns=['id', 'iterations', 'time_mean', 'time_std', 'size_mean', 'size_std'])
+
+for index, id in enumerate(ids):
+    precomputation_file = config.data_dir + id + '.xlsx'
+
+    try:
+        df = pd.read_excel(precomputation_file)
+    except Exception as e:
+        logging.warning('Unable to load precomputation file: '+precomputation_file+' (' + str(e) + ')')
+        continue
+
+    precomputation_results.loc[index] = [id, df.count(), df['time'].mean(), df['time'].std(), df['size'].mean(), df['size'].std()]
+
+precomputation_results.to_excel(config.final_result_dir+'precomputation.xlsx')
